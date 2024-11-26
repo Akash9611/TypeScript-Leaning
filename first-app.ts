25582e49202a40d59096dc7ea8794d803473a5cf
2works-Akash <akash@2works.io>
2024-11-26 16:52:06 +0530

    Type Inference and Explicit Type Annotation

--- first-app.ts
+++ first-app.ts
@@ -1,1 +1,17 @@
-nothing
+// TypeScript Inference and Explicit Type Annotation
+let userName = "Akash"; //Automatically set type as string
+
+userName = 34; // This line throw error because typescript uses strict type definition
+
+
+// The following code doesn't throw any error because it will fallback under the JavaScript code and the email doesn't know which type it has to explicitly prefer and it allows any types to be stored.  
+let email;
+email = "akash@gmail.com"
+email =34;
+//To resolve this above problem -We use Explicit type definition in TypeScript as follows
+// Explicit Type Definition
+let companyName: string;
+companyName = "Akash";
+
+// OR
+let companyName2:string = "Akash";
